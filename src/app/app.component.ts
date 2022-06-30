92daec3ba1465332da24d54fa6d0e0e5cad3e8c4
Willy Hille <willy.hille@intenta.de>
2022-06-30 12:57:36 +0200

    auto-scale on resize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-countdown';
+  @ViewChild("scale") div: any;
+  scale: number = 0;
+
+  constructor() {
+    window.addEventListener("resize", () => this.resize())
+  }
+
+  ngAfterViewInit(): void {
+    this.resize()
+  }
+
+  resize() {
+    let elm = this.div.nativeElement
+    this.scale = Math.min(window.innerWidth / elm.clientWidth, window.innerHeight / elm.clientHeight) * 0.75
+  }
+
+  getScale() {
+    return {
+      transform: "scale(" + this.scale + ")"
+    };
+  }
 }
